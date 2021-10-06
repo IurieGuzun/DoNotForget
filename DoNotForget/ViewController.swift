969a258e137f876077b987d1d1772dbfff0cef41
Iurie Guzun <iurie_guzun@yahoo.com>
2021-10-06 17:28:37 -0400

    Initial Commit

---
+++ DoNotForget/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DoNotForget
+//
+//  Created by Iurie Guzun on 2021-10-06.
+//  Copyright © 2021 Iurie Guzun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
